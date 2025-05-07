decaca9195f9a62e5d3b7a4060314a2ae1030739
Aslanian <aslanianandre@iMac-de-Aslanian.local>
2025-05-07 18:27:29 +0200

    Ajout de la page /setup

---
+++ app/setup/page.tsx
@@ -0,0 +1,10 @@
+// app/setup/page.tsx
+export default function SetupPage() {
+    return (
+      <div style={{ textAlign: "center", paddingTop: "50px" }}>
+        <h1>🔧 Configuration du cadenas</h1>
+        <p>Choisis un mot de passe pour ton cadenas BoxLock Connect</p>
+        {/* Ici tu mettras un formulaire plus tard */}
+      </div>
+    );
+  }
