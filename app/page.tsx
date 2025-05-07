9e081eee6ed91fd764e6d937151ab6ecfb9369c7
Aslanian <aslanianandre@iMac-de-Aslanian.local>
2025-05-07 18:22:59 +0200

    Ajout page d'accueil

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+// app/page.tsx
+export default function Home() {
+    return (
+      <div style={{ textAlign: "center", paddingTop: "50px" }}>
+        <h1>Bienvenue sur BoxLock Connect 🔓</h1>
+        <p>Utilise <code>/setup</code> ou <code>/access</code> pour démarrer</p>
+      </div>
+    );
+  }
