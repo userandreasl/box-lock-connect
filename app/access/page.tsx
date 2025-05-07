65bc891e70e8508036349b0e9b64b4de114a424b
Aslanian <aslanianandre@iMac-de-Aslanian.local>
2025-05-07 18:28:50 +0200

    Ajout de la page /access

---
+++ app/access/page.tsx
@@ -0,0 +1,10 @@
+// app/access/page.tsx
+export default function AccessPage() {
+    return (
+      <div style={{ textAlign: "center", paddingTop: "50px" }}>
+        <h1>🔓 Accès au cadenas</h1>
+        <p>Entre ton mot de passe pour ouvrir ton cadenas BoxLock Connect</p>
+        {/* Ici tu mettras la vérification du mot de passe plus tard */}
+      </div>
+    );
+  }
